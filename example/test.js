14e9f3236e3c92bd36297e4390d3babe56edb818
zhangxiang <958033967@qq.com>
2018-06-13 20:53:07 +0800

    done

--- example/test.js
+++ example/test.js
@@ -1,5 +1,13 @@
 const Request = require('../client');
 
 Request({
-  url: 'http://localhost:3000'
-});
+  url: 'http://localhost:3000',
+  qs: {
+    a: 123,
+    b: 456
+  }
+}).then((ret) => {
+  console.log(ret.headers);
+  console.log(ret.statusCode);
+  console.log(ret.body);
+}).catch((err) => { console.log(err); });
